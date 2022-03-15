ee04c6417153239eee644ed508f0f44e8398ab5f
Jessy <c.j91350@gmail.com>
2022-03-15 04:19:40 +0100

    Delete translation

--- src/application/use-cases/TranslationsUseCases.ts
+++ src/application/use-cases/TranslationsUseCases.ts
@@ -1,20 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { ITranslationsRepository } from "application/ports/ITranslationsRepository";
 import { Translation } from "domain/models/Translation";
 
 @Injectable()
 export class TranslationsUseCases {
-    constructor(private readonly translationsRepository: ITranslationsRepository) { }
+  constructor(private readonly translationsRepository: ITranslationsRepository) { }
 
-    async getAllTranslations(): Promise<Translation[]> {
-        return await this.translationsRepository.find()
-    }
+  async getAllTranslations(): Promise<Translation[]> {
+    return await this.translationsRepository.find()
+  }
 
-    async createNewTranslation(translation: Translation): Promise<Translation> {
-        try {
-            return await this.translationsRepository.save(translation)
-        } catch (error) {
-            throw Error(error)
-        }
+  async createNewTranslation(translation: Translation): Promise<Translation> {
+    try {
+      return await this.translationsRepository.save(translation)
+    } catch (error) {
+      throw Error(error)
     }
+  }
+
+  async removeTranslation(translationId: number): Promise<void> {
+    try {
+      const translation: Translation = await this.translationsRepository.findOne(translationId)
+      this.translationsRepository.delete(translation.id)
+    } catch (error) {
+      throw new NotFoundException(`The translation ${translationId} wasn't found.`);
+    }
+  }
 }
